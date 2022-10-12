4d587775de2a4201b2d4b809a0d61349fec3673a
Briana Reyes <112358632+briana142@users.noreply.github.com>
2022-10-12 08:36:30 -0500

    Create Arreglos1.js

---
+++ Arreglos1.js
@@ -0,0 +1,36 @@
+<html>
+<head>
+<title>Arreglos</title>
+</head>
+<body>
+<center>
+<h1>FLORES</h1>
+<table border="1" id="tablagato">
+<tr>
+<td>NOMBRE</td>
+<td>COLOR</td>
+<td>IMAGEN</td>
+</tr>
+<tr>
+<td>Girasol</td>
+<td>Amarillo</td>
+<td><img src="https://www.google.com.mx/url?sa=i&url=https%3A%2F%2Fplantsvszombies.fandom.com%2Fes%2Fwiki%2FDestello_Solar&psig=AOvVaw127EqkJa-gzUzkLFYaOwBV&ust=1665667240877000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCLCMtpjk2voCFQAAAAAdAAAAABAE"></td>
+</tr>
+<tr>
+<td>Rosa</td>
+<td>Roja</td>
+<td><img src="https://www.google.com.mx/url?sa=i&url=https%3A%2F%2Fsp.depositphotos.com%2Fstock-photos%2Fsola-rosa-roja.html&psig=AOvVaw3koOA9nj1fNCLpG7jB_QTx&ust=1665667384302000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCOjE2dzk2voCFQAAAAAdAAAAABAE"></td>
+</tr>
+<tr>
+<td>Clavel</td>
+<td>Matisado</td>
+<td><img src="https://www.google.com.mx/url?sa=i&url=https%3A%2F%2Fwww.pinterest.es%2Fpin%2F520236194435198476%2F%3Famp_client_id%3DCLIENT_ID(_)%26mweb_unauth_id%3D%26_url%3Dhttps%253A%252F%252Fwww.pinterest.es%252Famp%252Fpin%252F520236194435198476%252F%26from_amp_pin_page%3Dtrue&psig=AOvVaw1Lzm8kFtVM1DG_5dJZe86Y&ust=1665667519666000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCNDx2p3l2voCFQAAAAAdAAAAABAE"></td>
+</tr>
+<tr>
+<td>Bugambilia</td>
+<td>Rosa</td>
+<td><img src="https://www.google.com.mx/url?sa=i&url=https%3A%2F%2Fwww.eluniversal.com.mx%2Fmenu%2Fcuales-son-los-beneficios-de-la-infusion-de-bugambilia-para-la-salud&psig=AOvVaw0nbFwAMSf0fMOXeIeu5ZQb&ust=1665667617761000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCLiE9Mvl2voCFQAAAAAdAAAAABAE"></td>
+</tr>
+</table>
+</body>
+</html>
